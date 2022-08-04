cb051902f6a8a82188bc8b8c82ed6c122bd30109
sanazniamati <sanaz.niamati1991@gmail.com>
2022-08-04 13:32:05 +0430

    2

--- src/index.js
+++ src/index.js
@@ -1,50 +1,54 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ReactDOM from "react-dom/client";
-import { Stage, Layer, Shape, Text } from "react-konva";
+import { Stage, Layer, Shape, Text, Group } from "react-konva";
 
 const App = () => {
-  let [width1, updateWidth1] = useState(100);
-  const [width2, updateWidth2] = useState(80);
-  const [height1, updateHeight1] = useState(50);
-  const [height2, updateHeight2] = useState(30);
-  const [width, setWidth] = useState(0);
-  const [height, setHeight] = useState(0);
-  const [grad, setGrad] = useState(45);
+  const shapeRef = useRef();
   let [a, setA] = useState(100);
   let [b, setB] = useState(0);
   let [c, setC] = useState(0);
   let [d, setD] = useState(80);
   let [e, setE] = useState(50);
+  const [grad, setGrad] = useState(45);
   let initialQ = e - Math.tan((grad * Math.PI) / 180) * (a - d);
   let [q, setQ] = useState(initialQ);
-
+  let initialWidth1 = a - c;
+  let initialWidth2 = d - c;
+  let initialHeight1 = e - b;
+  let initialHeight2 = q - b;
+  let [width1, updateWidth1] = useState(initialWidth1);
+  const [width2, updateWidth2] = useState(initialWidth2);
+  const [height1, updateHeight1] = useState(initialHeight1);
+  const [height2, updateHeight2] = useState(initialHeight2);
+  useEffect(() => {
+    setQ(initialQ);
+    updateHeight2((height2) => q - b);
+  }, [grad]);
   const handelIncWidth = () => {
-    setWidth(width + 15);
+    // setWidth(width + 15);
     updateWidth1(width1 + 15);
     updateWidth2(width2 + 15);
     setA(a + 15);
     setD(d + 15);
   };
   const handelDecWidth = () => {
-    setWidth(width - 15);
+    // setWidth(width - 15);
     updateWidth1(width1 - 15);
     updateWidth2(width2 - 15);
     setA(a - 15);
     setD(d - 15);
   };
   const handelIncHeight = () => {
-    setHeight(height + 15);
     updateHeight1(height1 + 15);
     updateHeight2(height2 + 15);
     setE(e + 15);
     setQ(q + 15);
   };
-  const handelDecHeight = () => {
-    setHeight(height - 15);
+  const handelDecHeight = (e) => {
     updateHeight1(height1 - 15);
     updateHeight2(height2 - 15);
-    setE(e - 15);
-    setQ(q - 15);
+    // setE(e - 15);
+    // setQ(q - 15);
   };
   const handelIncDegree = () => {
     setGrad(grad + 5);
@@ -52,10 +56,17 @@ const App = () => {
   const handelDecDegree = () => {
     setGrad(grad - 5);
   };
-
+  const handelDragEnd = (e) => {
+    setC((c) => {
+      return e.target.x;
+    });
+    setB((b) => {
+      return e.target.y;
+    });
+  };
   return (
     <>
-      <div style={{ x: 250, y: 250 }}>
+      <div>
         <label>width : </label>
         <button onClick={handelIncWidth}>+</button>
         <button onClick={handelDecWidth}>-</button>
@@ -74,7 +85,7 @@ const App = () => {
       <Stage width={700} height={700}>
         <Layer>
           <Text
-            x={250}
+            x={100}
             y={250}
             fontSize={20}
             text={
@@ -90,57 +101,62 @@ const App = () => {
               e +
               "  q:" +
               q.toFixed(0) +
-              ""
+              "  grad:" +
+              grad
             }
           />
-          <Text
-            x={(a - c) / 2}
-            y={b}
-            text={width1}
-            fontSize={12}
-            fill={"red"}
-          />
-          <Text
-            x={(d - c) / 2}
-            y={e + 10}
-            text={width2}
-            fontSize={12}
-            fill={"red"}
-          />
+          <Group draggable>
+            <Text
+              x={(a - c) / 2}
+              y={b}
+              text={width1}
+              fontSize={12}
+              fill={"red"}
+            />
+            <Text
+              x={(d - c) / 2}
+              y={e + 10}
+              text={width2}
+              fontSize={12}
+              fill={"red"}
+            />
 
-          <Text
-            x={c}
-            y={(e - b) / 2}
-            text={height1}
-            fontSize={10}
-            fill={"red"}
-          />
-          <Text
-            x={a + 15}
-            y={(q - b) / 2}
-            text={height2}
-            fontSize={12}
-            fill={"red"}
-          />
-          <Shape
-            x={10}
-            y={10}
-            sceneFunc={(context, shape) => {
-              context.beginPath();
-              context.moveTo(a + width, b);
-              context.lineTo(a, b);
-              context.lineTo(c, b);
-              context.lineTo(c, e + height);
-              context.lineTo(d + width, e + height);
-              context.lineTo(a + width, q + height);
-              context.closePath();
-              context.fillStrokeShape(shape);
-            }}
-            fill="green"
-            stroke="black"
-            strokeWidth={2}
-            draggable
-          />
+            <Text
+              x={c}
+              y={(e - b) / 2}
+              text={height1}
+              fontSize={10}
+              fill={"red"}
+            />
+            <Text
+              x={a + 15}
+              y={(q - b) / 2}
+              text={height2}
+              fontSize={12}
+              fill={"red"}
+            />
+            <Shape
+              ref={shapeRef}
+              x={10}
+              y={10}
+              onDragEnd={handelDragEnd}
+              sceneFunc={(context, shape) => {
+                context.beginPath();
+                context.moveTo(a, b);
+                context.lineTo(a, b);
+                context.lineTo(c, b);
+                context.lineTo(c, e);
+                context.lineTo(d, e);
+                context.lineTo(a, q);
+                context.closePath();
+                context.fillStrokeShape(shape);
+              }}
+              fill="green"
+              stroke="black"
+              strokeWidth={2}
+              // draggable
+            />
+          </Group>
         </Layer>
       </Stage>
     </>
